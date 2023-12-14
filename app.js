71cc0c58f51f413fbf1ef5b0312e39c932d8fbf2
Dan <danporeda@yahoo.com>
2023-12-14 15:57:16 -0500

    refactored using a class

--- app.js
+++ app.js
@@ -1,17 +1,77 @@
-function makeColor(r,g,b) {
-  const color = {};
-  color.r = r;
-  color.g = g;
-  color.b = b;
-  color.rgb = function(){
+// function makeColor(r,g,b) {
+//   const color = {};
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
+//   color.rgb = function(){
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   };
+//   color.hex = function() {
+//     const { r, g, b } = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   }
+//   return color;
+// }
+
+// const firstColor = makeColor(35,255,150);
+
+
+/*CONSTRUCTOR FUNCTION */
+// function Color(r,g,b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// Color.prototype.rgb = function() {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// Color.prototype.hex = function() {
+//   const { r, g, b } = this;
+//   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// Color.prototype.rgba = function(a=1.0) {
+//   const { r, g, b } = this;
+//   return `rgba(${r},${g},${b},${a})`;
+// }
+
+// const firstColor = new Color(255,5,3);
+// const color1 = new Color(255,0,0);
+// const color2 = new Color(0,0,255);
+
+/* REFACTORED WITH CLASS */
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+
+  innerRGB(){
     const { r, g, b } = this;
-    return `rgb(${r}, ${g}, ${b})`;
-  };
-  color.hex = function() {
+    return `${r},${g},${b}`;
+  }
+
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+
+  rgba(a=1.0) {
+    return `rgba(${this.innerRGB()},${a})`;
+  }
+
+  hex() {
     const { r, g, b } = this;
     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
-  return color;
+
 }
 
-const firstColor = makeColor(35,255,150);
+const color1 = new Color(255, 67, 89, 'tomato');
+
